fa6051ca15c05dd3ae536acaa9877392ba34b20c
egemen inceler <egemeninceler@gmail.com>
2021-07-29 22:10:21 +0300

    First commit

---
+++ incelerExtesions/ViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension UIView{
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+}
