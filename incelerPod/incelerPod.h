dce6b5a873f0e6dcccef9a9e95bfaca63f527d47
egemen inceler <egemeninceler@gmail.com>
2021-07-29 22:53:59 +0300

    file added

---
+++ incelerPod/incelerPod.h
@@ -0,0 +1,18 @@
+//
+//  incelerPod.h
+//  incelerPod
+//
+//  Created by Egemen Inceler on 29.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for incelerPod.
+FOUNDATION_EXPORT double incelerPodVersionNumber;
+
+//! Project version string for incelerPod.
+FOUNDATION_EXPORT const unsigned char incelerPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <incelerPod/PublicHeader.h>
+
+
